1a7112de0de16f623f06ef5d5bfa73738f8d32ca
Serhii-Norda <djudomma@gmail.com>
2024-09-11 20:16:23 +0300

    test

---
+++ tests/IF-else.spac.js
@@ -0,0 +1,17 @@
+if(true){
+    console.log(true)
+}
+
+if(10>1){
+    console.log('some text')
+};
+
+if(10==10){
+    console.log('10=10')
+}else{
+    console.log('10 no equal 10')
+};
+
+if('some text'){
+    
+}   
---
+++ tests/operators.spec.js
@@ -0,0 +1,32 @@
+// операторы сравнения
+
+/*
+    > - слева больше чем справа
+    < - справа больше чем слева
+    = - присвоение 
+    == - соответствие значения справа - значению слева
+    === - строгое сравнение, все должно отвечать всему включая и тип данных
+    >= - больше либо равно
+    <= - меньше или равно
+    != - не равно
+*/
+
+let someVariable = 1;
+const someVariable2 = true;
+
+console.log(someVariable == someVariable2);
+
+const firstNumver = 1;
+const secondNumver = 10;
+
+let result = firstNumver < secondNumver
+
+/*
+в Тайп скрипте все ниже приведенное воспринимаеться как false
+0
+NaN
+undefined
+null
+''
+
+*/
