fd3e30e187b254224714fff84b4099db85167611
Serhii-Norda <djudomma@gmail.com>
2024-09-11 20:16:09 +0300

    arrow function

---
+++ tests/Function.spec.ts
@@ -0,0 +1,20 @@
+// ключевое слово function
+// название функции 
+// () - в которых передаються параметры, название параметра
+// {} - блок кода которій будет віполняться
+// return - ключевое слово которое указівает что результат исполнения кода нужно вернуть 
+
+const someFunc = (param1, param2)=>{
+    const someVariable = 'Hello'
+};
+
+function somFunc2(param1: any, param2: any){
+    return '' + someVariable2
+};
+
+const someVariable2 = 'Hello'
+
+// три типа функций
+// function declaration     function name(param) { retern ''}
+// arrow function   ()=>{}   ()=>.....
+// function expression можна заасайнить на переменную const someExpression = function(){}
